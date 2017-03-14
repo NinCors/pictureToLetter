b92eb199c5e55b22712ac43fb5e0e5b825611082
chiyu <chiyuc2@gmail.com>
2017-03-13 23:36:55 -0700

    use argparser libray to manange user input

--- ptl.py
+++ ptl.py
@@ -3,10 +3,18 @@ import argparse
 
 ascii_letter = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 
+# Input argument parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')    # input file
+parser.add_argument('-o', '--output')   #output file
+parser.add_argument('--width', type = int, default = 80) # width of output letter graph
+parser.add_argument('--height', type = int, default = 80) # height of output letter grap
+
+
 
 
 # convert RGB to letter
-
 def get_char(r,g,b,alpha = 256):
     if alpha == 0:
         return ' '
